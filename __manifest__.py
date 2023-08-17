f0c22031ed8fc045e4ff668b339275734be9b372
Ignacio Buioli <ibuioli@gmail.com>
2023-08-17 09:10:56 -0300

    feat: initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Add filter location ',
+    'version': '14.0',
+    'depends': [
+        'base','stock','stock_account',
+    ],
+    'author': 'Mauro Bogado, Exemax',
+    'website': 'www.exemax.com.ar',
+    'summary': 'Modulo de filtrar por ubicaciones en valuaciones de inventario',
+    'category': 'Extra Tools',
+    'description': '''
+    Modulo de filtrar por ubicaciones en valuaciones de inventario.
+    ''',
+    'data': [
+        'views/add_filter_location.xml',
+       
+    ],
+}
---
+++ models/add_filter_location.py
@@ -0,0 +1,9 @@
+
+from odoo import models, api, fields
+
+
+class AddFilterLocation(models.Model):
+    
+    _inherit = 'product.product'
+    
+    
